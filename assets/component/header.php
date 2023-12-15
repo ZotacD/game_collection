4bbc50a27f3ba050ef694fb35ab97b3b9daa3966
ewen <ewenbuhot@outlook.fr>
2023-12-15 11:38:31 +0100

    update

--- assets/component/header.php
+++ assets/component/header.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <header>
-        <a href="index.php">Homeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</a>
+        <a href="index.php">Home</a>
         <div id="nav_right">
             <a href="index.php">MA BIBLIOTHÈQUE</a>
             <a href="index.php">AJOUTER UN JEU</a>
