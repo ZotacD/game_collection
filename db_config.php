254d90da038e5d55830fca6f2f5713e4a4424e71
ZotacD <tom.delangle@outlook.fr>
2023-12-15 12:24:29 +0100

    update user model

--- db_config.php
+++ db_config.php
@@ -1,6 +1,7 @@
 <?php
-    function db_config(){
-        $bdd = new PDO('mysql:host=' . $_ENV["DB_HOST"] . ';dbname=' . $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-        return $bdd;
-    }
+function dbConfig()
+{
+    $bdd = new PDO('mysql:host=' . $_ENV["DB_HOST"] . ';dbname=' . $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+    return $bdd;
+}
 ?>
