63522d374c3ccafcc4b6acd01b4aaa75a4e3309a
ZotacD <thomas.longjohn14@gmail.com>
2023-12-15 11:02:43 +0100

    Init

--- app/view/example.php
+++ app/view/example.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <base href="<?php echo $_ENV["BASE_DIR"] ?>">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+// Récupérer l'URL depuis le paramètre "url"
+$requestUrl = isset($_GET['url']) ? $_GET['url'] : '/';
+
+switch ($requestUrl) {
+    case '/':
+        require_once 'app/controller/home.php';
+        break;
+    case '403':
+        var_dump("403");
+        break;
+    case '404':
+        var_dump("404");
+        break;
+    default:
+        header("Location: " . $_ENV["BASE_DIR"] . "404");
+        exit();
+}
