363cd968ab96495c7cb804606f98111c2ce6a6f7
ZotacD <thomas.longjohn14@gmail.com>
2023-12-15 10:14:41 +0100

    Init

---
+++ app/controller/example.php
@@ -0,0 +1,14 @@
+<?php
+
+// Récupérer l'URL depuis le paramètre "url"
+$requestUrl = isset($_GET['endpoint']) ? $_GET['endpoint'] : '/';
+
+switch ($requestUrl) {
+    case '/':
+        require_once "model/example.php";
+        require_once "view/example.php";
+        break;
+    default:
+        header("Location: " . "" . "/404");
+        exit();
+}
---
+++ app/model/example.php
@@ -0,0 +1,16 @@
+<?php
+
+function addUser()
+{
+
+}
+
+function updateUser()
+{
+
+}
+
+function removeUser()
+{
+
+}
