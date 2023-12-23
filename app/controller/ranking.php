fbe999d24be1c101a29cfb4fd1912ca13dd812d5
ZotacD <tom.delangle@outlook.fr>
2023-12-23 01:27:22 +0100

    Add ranking page redirection

---
+++ app/controller/ranking.php
@@ -0,0 +1,14 @@
+<?php
+
+// Récupérer l'URL depuis le paramètre "url"
+$requestUrl = isset($_GET['endpoint']) ? $_GET['endpoint'] : '/';
+
+switch ($requestUrl) {
+    case '/':
+        require_once "app/model/user.php";
+        require_once "app/view/ranking.php";
+        break;
+    default:
+        header("Location: " . $_ENV["BASE_DIR"] . "404");
+        exit();
+}
---
+++ app/view/ranking.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <base href="<?php echo $_ENV["BASE_DIR"] ?>">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/utils.css">
+    <link rel="stylesheet" href="assets/css/ranking.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php require_once "app/view/component/header.php"; ?>
+
+    <?php require_once "app/view/component/footer.php"; ?>
+</body>
+
+</html>
