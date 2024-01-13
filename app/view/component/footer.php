2df4a0227635ea928353f2c359f4b4dacadb6db7
ZotacD <tom.delangle@outlook.fr>
2024-01-13 16:56:17 +0100

    refactoring header and footer style

--- app/view/component/footer.php
+++ app/view/component/footer.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <base href="<?php echo $_ENV["BASE_DIR"] ?>">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/utils.css">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="footer">Game Collection - 2023 - Tous droits réservés</div>
-</body>
-
-</html>
+<div class="footer">Game Collection - 2023 - Tous droits réservés</div>
--- app/view/component/header.php
+++ app/view/component/header.php
@@ -1,25 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <base href="<?php echo $_ENV["BASE_DIR"] ?>">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/header.css">
-    <link rel="stylesheet" href="assets/css/utils.css">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="header">
-        <a href="">Home</a>
-        <div id="nav_right">
-            <a href="library">MA BIBLIOTHÈQUE</a>
-            <a href="library/add">AJOUTER UN JEU</a>
-            <a href="ranking">CLASSEMENT</a>
-            <a href="auth">PROFIL</a>
-        </div>
+<div class="header">
+    <a href="">Home</a>
+    <div id="nav_right">
+        <a href="library">MA BIBLIOTHÈQUE</a>
+        <a href="library/add">AJOUTER UN JEU</a>
+        <a href="ranking">CLASSEMENT</a>
+        <a href="auth">PROFIL</a>
     </div>
-</body>
-
-</html>
+</div>
